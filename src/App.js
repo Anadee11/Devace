481c3d1efcd02d14df2ab2fbdbae7894896f3be5
Anadee11 <anadeeyadav46@gmail.com>
2022-09-18 16:17:51 +0530

    Final Push

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css'; 
+import Header from './Components/Header' 
+import HomePage from './Components/HomePage'; 
+import Test from './Components/Test'; 
+import Resources from './Components/Resources'; 
+import Symptom from './Components/Symptom'; 
+import ContactUs from './Components/ContactUs/ContactUs'; 
+import Footer from './Components/Footer/Footer'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom' ;
+import AfterRoom from './Components/AfterRoom/AfterRoom'; 
+ 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <BrowserRouter> 
+        <Header /> 
+        <Routes> 
+          <Route path='/' element={<HomePage />} /> 
+          <Route path='/test' element={<Test />} /> 
+          <Route path='/symptom' element={<Symptom />} /> 
+          <Route path='/resources' element={<Resources />} /> 
+          <Route path='/contact' element={<ContactUs />} /> 
+          <Route path='/after' element={<AfterRoom />} /> 
+        </Routes> 
+        <Footer /> 
+      </BrowserRouter> 
+      {/* <Navbar /> 
+      <HomePage /> 
+      <Test /> 
+      <Symptom /> 
+      <Resources /> 
+      <ContactUs /> 
+      <Footer /> */} 
+    </div> 
+  ); 
+} 
+ 
+export default App;
